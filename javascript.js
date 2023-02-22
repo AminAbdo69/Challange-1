b5ecca1cd4ff0f71adeee0a2ac1893a173e503ce
AminAbdo <AminAbdo69>
2023-02-23 00:39:34 +0200

    push all

---
+++ javascript.js
@@ -0,0 +1,17 @@
+const menu = document.querySelector("header .logo .toggle");
+let uls = document.querySelector("header nav ul li");
+let land = document.querySelector(".landing");
+// land.style.backgroundColor="black";
+
+menu.onclick = function () {
+    document.body.backgroundColor="black";
+    // land.style.backgroundImage="none";
+    // land.style.backgroundcolor="black";
+    // if( menu.classList.toggle("active")) {
+    //     uls.style.display="flex";
+    // }
+    // else {
+    //     uls.style.display="none";
+
+    // };
+};
