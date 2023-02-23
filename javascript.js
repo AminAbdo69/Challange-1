67f91808fed3f5632a001005ba69e0d93ecb212b
Amin_Abdo <117170745+AminAbdo69@users.noreply.github.com>
2023-02-23 02:12:22 +0200

    Update javascript.js
    add toggle menu effect

--- javascript.js
+++ javascript.js
@@ -1,17 +1,19 @@
-const menu = document.querySelector("header .logo .toggle");
-let uls = document.querySelector("header nav ul li");
+let togglemen = document.querySelector(".toggle");
+let uls =document.querySelector("header nav ul");
 let land = document.querySelector(".landing");
-// land.style.backgroundColor="black";
+let cen = document.querySelector(".landing .text");
 
-menu.onclick = function () {
-    document.body.backgroundColor="black";
-    // land.style.backgroundImage="none";
-    // land.style.backgroundcolor="black";
-    // if( menu.classList.toggle("active")) {
-    //     uls.style.display="flex";
-    // }
-    // else {
-    //     uls.style.display="none";
 
-    // };
-};
+togglemen.onclick = function () {
+    land.style.backgroundImage="none";
+    land.style.backgroundColor="black";
+    cen.style.display="none";
+    if( togglemen.classList.toggle("active")) {
+        uls.style.display="flex";
+    }
+    else {
+        uls.style.display="none";
+        land.style.backgroundImage="url('/images/mobile/image-hero.jpg')";
+        cen.style.display="block";
+    }
+}
